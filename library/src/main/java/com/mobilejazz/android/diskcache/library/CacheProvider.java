c3340899c7ce36507fe28d8db39f5a5a144671ed
Hannes Widmoser <widmoser@gmail.com>
2015-07-21 12:56:12 +0200

    added a cache provider

---
+++ library/src/main/java/com/mobilejazz/android/diskcache/library/CacheProvider.java
@@ -0,0 +1,10 @@
+package com.mobilejazz.android.diskcache.library;
+
+import java.io.FileNotFoundException;
+
+import android.net.Uri;
+import android.os.ParcelFileDescriptor;
+import android.support.v4.content.FileProvider;
+
+public class CacheProvider extends FileProvider {
+}
