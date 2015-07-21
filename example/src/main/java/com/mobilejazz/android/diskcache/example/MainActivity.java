ef3082f900151ea3f078b5383afb76538306958a
Hannes Widmoser <widmoser@gmail.com>
2015-07-21 18:53:33 +0200

    [example] setting up buttons do add a file and clear the cache manually

--- example/src/main/java/com/mobilejazz/android/diskcache/example/MainActivity.java
+++ example/src/main/java/com/mobilejazz/android/diskcache/example/MainActivity.java
@@ -1,11 +1,19 @@
 package com.mobilejazz.android.diskcache.example;
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.DatePicker;
 
-public class MainActivity extends Activity {
+import com.mobilejazz.android.diskcache.library.CacheProvider;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+public class MainActivity extends Activity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +41,24 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addFile(View v) {
+        Calendar c = Calendar.getInstance();
+        DatePickerDialog d = new DatePickerDialog(this, this, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH));
+        d.show();
+    }
+
+    public void clear(View v) {
+        try {
+            CacheProvider.clear(this, getString(R.string.authority_cache));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+        // TODO: add a file
     }
 }
