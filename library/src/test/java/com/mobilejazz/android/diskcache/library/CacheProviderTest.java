b4a75052192d8063eb46932e12f4cba0f5bad8de
Hannes Widmoser <widmoser@gmail.com>
2015-07-21 17:54:29 +0200

    added a test case

---
+++ library/src/test/java/com/mobilejazz/android/diskcache/library/CacheProviderTest.java
@@ -0,0 +1,100 @@
+package com.mobilejazz.android.diskcache.library;
+
+import java.io.File;
+
+import android.content.Context;
+import android.content.pm.ProviderInfo;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.test.AndroidTestCase;
+import android.test.IsolatedContext;
+import android.test.RenamingDelegatingContext;
+import android.test.mock.MockContentResolver;
+import android.test.mock.MockContext;
+
+public abstract class CacheProviderTestCase extends AndroidTestCase {
+
+    String mProviderAuthority;
+
+    private IsolatedContext mProviderContext;
+    private MockContentResolver mResolver;
+
+    private class MockContext2 extends MockContext {
+
+        @Override
+        public Resources getResources() {
+            return getContext().getResources();
+        }
+
+        @Override
+        public File getDir(String name, int mode) {
+            // name the directory so the directory will be separated from
+            // one created through the regular Context
+            return getContext().getDir("mockcontext2_" + name, mode);
+        }
+
+        @Override
+        public Context getApplicationContext() {
+            return this;
+        }
+    }
+
+    public CacheProviderTestCase() {
+        mProviderAuthority = "com.mobilejazz.android.diskcache.library.CACHE_PROVIDER";
+    }
+
+    private CacheProvider mProvider;
+
+    public CacheProvider getProvider() {
+        return mProvider;
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        mResolver = new MockContentResolver();
+        final String filenamePrefix = "test.";
+        RenamingDelegatingContext targetContextWrapper = new RenamingDelegatingContext(new MockContext2(), // The
+                                                                                                           // context
+                                                                                                           // that
+                                                                                                           // most
+                                                                                                           // methods
+                                                                                                           // are
+                // delegated to
+                getContext(), // The context that file methods are delegated to
+                filenamePrefix);
+        mProviderContext = new IsolatedContext(mResolver, targetContextWrapper);
+        mProvider = createProviderForTest(mProviderContext, mProviderAuthority);
+        mResolver.addProvider(mProviderAuthority, getProvider());
+    }
+
+    static CacheProvider createProviderForTest(Context context, String authority) throws IllegalAccessException, InstantiationException {
+        CacheProvider instance = new CacheProvider();
+        ProviderInfo providerInfo = new ProviderInfo();
+        providerInfo.authority = authority;
+        providerInfo.metaData = new Bundle();
+        providerInfo.metaData.putInt("android.support.FILE_PROVIDER_PATHS", R.xml.cache_filepath_single);
+        instance.attachInfoForTesting(context, providerInfo);
+        return instance;
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        mProvider.shutdown();
+        super.tearDown();
+    }
+
+    public MockContentResolver getMockContentResolver() {
+        return mResolver;
+    }
+
+    public IsolatedContext getMockContext() {
+        return mProviderContext;
+    }
+
+    public void testSomething() {
+        assertEquals(true, false);
+    }
+
+}
