1059a46ced062f1b3f5ff059e51eadcdbfb30618
Hannes Widmoser <widmoser@gmail.com>
2015-07-21 16:00:54 +0200

    executing a clear of the cache directories asynchronously

--- library/src/main/java/com/mobilejazz/android/diskcache/library/CacheBroadcastReceiver.java
+++ library/src/main/java/com/mobilejazz/android/diskcache/library/CacheBroadcastReceiver.java
@@ -3,16 +3,22 @@ package com.mobilejazz.android.diskcache.library;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 
 public class CacheBroadcastReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        try {
-            CacheProvider.clear(context, intent.getStringExtra(CacheProvider.EXTRA_AUTHORITY));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public void onReceive(final Context context, final Intent intent) {
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    CacheProvider.clear(context, intent.getStringExtra(CacheProvider.EXTRA_AUTHORITY));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+        }.execute();
     }
-
 }
